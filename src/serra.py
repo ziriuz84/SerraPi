1df30934cea9d076e3e3e8d7466ac46723158ea2
Sirio Negri <ziriuz84@gmail.com>
2016-06-16 19:36:54 +0200

    Aggiunta di interfaccia base di urwid e multithreading

--- src/serra.py
+++ src/serra.py
@@ -5,14 +5,29 @@ import time
 import RPi.GPIO as GPIO
 from functions import ConfigSectionMap, Initialize
 import SensoreTRH
+import urwid
+import thread
 
 
-def main():
-    DBPosition = ConfigSectionMap("DATABASE")['posizione']
-    DBUser = ConfigSectionMap("DATABASE")['utente']
-    DBPassword = ConfigSectionMap("DATABASE")['password']
-    DBName = ConfigSectionMap("DATABASE")['nome']
-    con = Initialize(DBPosition, DBUser, DBPassword, DBName)
+def exit(scelta):
+    if scelta in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+
+
+def interfaccia(con):
+    print 1
+    try:
+        thread.start_new_thread(misura, (con, ))
+    except:
+        "errore nell'apertura dei thread"
+    txt = urwid.Text(u"SerraPi 0.1")
+    fill = urwid.Filler(txt, 'top')
+    loop = urwid.MainLoop(fill, unhandled_input=exit)
+    loop.run()
+
+
+def misura(con):
+    print 2
     PinTInt = ConfigSectionMap("PIN")['temperaturainterna']
     SensoreTInt = ConfigSectionMap("PIN")['sensoretemperaturainterna']
     GPIO.setmode(GPIO.BCM)
@@ -24,4 +39,9 @@ def main():
     GPIO.cleanup()
 
 if __name__ == "__main__":
-    main()
+    DBPosition = ConfigSectionMap("DATABASE")['posizione']
+    DBUser = ConfigSectionMap("DATABASE")['utente']
+    DBPassword = ConfigSectionMap("DATABASE")['password']
+    DBName = ConfigSectionMap("DATABASE")['nome']
+    con = Initialize(DBPosition, DBUser, DBPassword, DBName)
+    interfaccia(con)
