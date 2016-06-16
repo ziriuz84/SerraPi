51fa8707c66ae1c1ad5de20c1c883f7927955f86
Sirio Negri <ziriuz84@gmail.com>
2016-06-16 16:56:14 +0200

    Aggiunta la funzione main()

--- src/serra.py
+++ src/serra.py
@@ -6,17 +6,22 @@ import RPi.GPIO as GPIO
 from functions import ConfigSectionMap, Initialize
 import SensoreTRH
 
-DBPosition = ConfigSectionMap("DATABASE")['posizione']
-DBUser = ConfigSectionMap("DATABASE")['utente']
-DBPassword = ConfigSectionMap("DATABASE")['password']
-DBName = ConfigSectionMap("DATABASE")['nome']
-con = Initialize(DBPosition, DBUser, DBPassword, DBName)
-PinTInt = ConfigSectionMap("PIN")['temperaturainterna']
-SensoreTInt = ConfigSectionMap("PIN")['sensoretemperaturainterna']
-GPIO.setmode(GPIO.BCM)
-SensoreInterno = SensoreTRH.SensoreTRH(SensoreTInt, PinTInt)
-SensoreInterno.SelectType()
-while True:
-    SensoreInterno.Misure(con)
-    time.sleep(10)
-GPIO.cleanup()
+
+def main():
+    DBPosition = ConfigSectionMap("DATABASE")['posizione']
+    DBUser = ConfigSectionMap("DATABASE")['utente']
+    DBPassword = ConfigSectionMap("DATABASE")['password']
+    DBName = ConfigSectionMap("DATABASE")['nome']
+    con = Initialize(DBPosition, DBUser, DBPassword, DBName)
+    PinTInt = ConfigSectionMap("PIN")['temperaturainterna']
+    SensoreTInt = ConfigSectionMap("PIN")['sensoretemperaturainterna']
+    GPIO.setmode(GPIO.BCM)
+    SensoreInterno = SensoreTRH.SensoreTRH(SensoreTInt, PinTInt)
+    SensoreInterno.SelectType()
+    while True:
+        SensoreInterno.Misure(con)
+        time.sleep(10)
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()
